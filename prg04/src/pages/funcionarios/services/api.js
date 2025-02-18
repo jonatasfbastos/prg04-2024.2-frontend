958c47850cfe5f4e456578eb2f93780833c9ca00
RianSantos09 <riansantosn45@gmail.com>
2025-02-18 09:08:44 -0300

    add  localhost funcionario

--- prg04/src/pages/funcionarios/services/api.js
+++ prg04/src/pages/funcionarios/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '', // Substitua pela URL do seu backend
+  baseURL: 'http://localhost:8080/funcionario', // Substitua pela URL do seu backend
   headers: {
     'Content-Type': 'application/json',
   },
