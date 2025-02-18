575713fee27c3034d7ff48b452c3ae1414b366e0
RianSantos09 <riansantosn45@gmail.com>
2025-02-18 08:43:58 -0300

    Create index.jsx

---
+++ prg04/src/pages/funcionarios/index.jsx
@@ -0,0 +1,14 @@
+import 'react';
+
+
+const funcionarios = () => {
+  return (
+    <div>
+      <h1>Gestão de Funcionários</h1>
+      <cadastrofuncionario />
+      <tablefuncionario />
+    </div>
+  );
+};
+
+export default funcionarios;
